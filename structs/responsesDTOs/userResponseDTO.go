cdb06b17b5c1dbe29ca424d5d5176447811275ee
Bede Abbe <bede.abbe@gmail.com>
2024-08-27 00:12:47 +0000

    changes

--- structs/responsesDTOs/userResponseDTO.go
+++ structs/responsesDTOs/userResponseDTO.go
@@ -3,7 +3,7 @@ package responsesDTOs
 import "authentication_service/models"
 
 type UserResponseDTO struct {
-	StatusCode int           `orm: "omitempty"`
-	User       *models.Users `orm: "omitempty"`
-	StatusDesc string        `orm:"size(255)"`
+	StatusCode int
+	User       *models.Users
+	StatusDesc string
 }
