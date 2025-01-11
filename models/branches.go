36f865ddac22ae6212d4106b55a62a8592a7b113
Bede Abbe <bede.abbe@gmail.com>
2025-01-11 15:50:41 +0100

    changes

--- models/branches.go
+++ models/branches.go
@@ -13,7 +13,7 @@ import (
 type Branches struct {
 	BranchId     int64      `orm:"auto"`
 	Branch       string     `orm:"size(80)"`
-	Country      *Countries `orm:"rel(fk);column(country)"`
+	Country      *Countries `orm:"rel(fk);column(country_id)"`
 	Location     string
 	PhoneNumber  string
 	Active       int       `orm:"omitempty"`
